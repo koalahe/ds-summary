43d1a3b13867811da0f98fa4f069b05b2855fbc2
Connie He <shiyuhe@andrew.cmu.edu>
2022-07-25 23:21:15 -0400

    add customized compare

--- summary-1.py
+++ summary-1.py
@@ -1,2 +1,11 @@
-#test
-#test 2
+import functools
+
+'''Customized sort list for python 3'''
+def customizedSort(x,y):
+    return(x ** 3 - y ** 3)
+
+if __name__ == "__main__":
+    list_1 = [6, 10, 5, 20, 46]
+    sorted_l = sorted(list_1, key=functools.cmp_to_key(customizedSort))
+    print(sorted_l)
+
